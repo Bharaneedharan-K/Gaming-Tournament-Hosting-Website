110ec63b401c7cb8d52800839b9ccb7d974e8aa8
Bharaneedharan-K <bharaneedharan2004@gmail.com>
2025-02-09 10:42:04 +0530

    first commit

---
+++ login/script.js
@@ -0,0 +1,9 @@
+var container = document.querySelector(".container");
+
+function showRegister() {
+    container.classList.add("show-register");
+}
+
+function showLogin() {
+    container.classList.remove("show-register");
+}
