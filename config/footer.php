e0bb95e780bef684549dba091dd131143757aa94
Bharaneedharan-K <bharaneedharan2004@gmail.com>
2025-03-27 17:55:06 +0530

    Create the footer

---
+++ config/footer.php
@@ -0,0 +1,33 @@
+    </div>
+    <footer class="bg-dark text-light py-4 mt-5 border-top border-primary">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6">
+                    <h5>EpicClash Gaming Tournaments</h5>
+                    <p>Your ultimate destination for competitive gaming tournaments.</p>
+                </div>
+                <div class="col-md-6 text-md-end">
+                    <h5>Connect With Us</h5>
+                    <div class="social-links">
+                        <a href="#" class="text-light me-3"><i class="fab fa-facebook"></i></a>
+                        <a href="#" class="text-light me-3"><i class="fab fa-twitter"></i></a>
+                        <a href="#" class="text-light me-3"><i class="fab fa-instagram"></i></a>
+                        <a href="#" class="text-light"><i class="fab fa-discord"></i></a>
+                    </div>
+                </div>
+            </div>
+            <hr class="my-4">
+            <div class="text-center">
+                <p class="mb-0">&copy; <?php echo date('Y'); ?> EpicClash. All rights reserved.</p>
+            </div>
+        </div>
+    </footer>
+
+    <!-- Bootstrap JS -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+    <!-- jQuery -->
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <!-- Custom JS -->
+    <script src="assets/js/main.js"></script>
+</body>
+</html> 
